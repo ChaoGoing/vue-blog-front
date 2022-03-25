0f745a2ff9628ad017792ea1c1f1320b39dca35b
陈子超 <chenzichao@ydjdev.com>
2022-03-25 17:51:47 +0800

    chore: pnpm & pinia & eslint import报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,38 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "airbnb-base",
-        'plugin:prettier/recommended' // 添加 prettier 插件
-    ],
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+    'plugin:prettier/recommended' // 添加 prettier 插件
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.ts', '.js', '.jsx', '.tsx'] // 可忽略的后缀名
+      }
     }
-};
+  },
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
-import store from './store/index'
 
-import ElementPlus from 'element-plus'
 // import 'element-plus/lib/theme-chalk/index.css'
 
 const app = createApp(App)
-app.use(router)
-.use(store)
-.use(ElementPlus)
+const pinia = createPinia()
+
+app.use(router).use(pinia).use(ElementPlus)
 
 app.mount('#app')
